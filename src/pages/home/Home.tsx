45791a362aceda53b6939506c11b28b4a334e01e
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-11-20 22:29:03 +0100

    post message

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -23,8 +23,9 @@ export class Home extends React.Component<any, StateType> {
     event.preventDefault();
     console.log("tello");
     ApiService.postMessage(this.state.author, this.state.body, (success: ApiSuccess, error: ApiError) => {
-      if (error) return alert("Error");
+      if (error) return alert("Error sending message. Please try again!");
       this.setState({ body: "" });
+      alert("Message sent successfully!");
     });
   }
 
@@ -39,20 +40,27 @@ export class Home extends React.Component<any, StateType> {
 
   render() {
     return (
-      <div>
-        <h1>Welcome to ChifLive</h1>
-        <h2>Add your comment to ChifLive now!</h2>
-        <div>
-          <form onSubmit={(event) => this.sendMessage(event)}>
-            <label>
-              Author:
-              <input type="text" value={this.state.author} onChange={(event) => this.authorChanged(event)} />
-            </label>
-            <label>
-              Body:
-              <input type="text" value={this.state.body} onChange={(event) => this.bodyChanged(event)} />
-            </label>
-            <input type="submit" value="Submit" />
+      <div className="container">
+        <h1 className="title">CHIFLIVE</h1>
+        <h2 className="subtitle">ADD YOUR COMMENT TO CHIFLIFE NOW!</h2>
+        <div className="form-container">
+          <form className="form-element" onSubmit={(event) => this.sendMessage(event)}>
+            <input
+              className="author-input"
+              placeholder="Author (optional)"
+              type="text"
+              value={this.state.author}
+              onChange={(event) => this.authorChanged(event)}
+            />
+            <textarea
+              className="body-input"
+              placeholder="Your Message"
+              value={this.state.body}
+              onChange={(event) => this.bodyChanged(event)}
+            />
+            <button className="button-input" type="submit">
+              Post Message
+            </button>
           </form>
         </div>
       </div>
